978026d9f36b5d45efa7d287ce619d1499170e82
Star Brilliant <coder@poorlab.com>
2019-06-09 10:29:06 +0800

    Initial commit

---
+++ pmtud-injector.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2019 Star Brilliant
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import expiringdict
+import functools
+import ipaddress
+import logging
+import scapy.all
+import sys
+from typing import *
+
+
+class Rule:
+    def __init__(self, src: Union[ipaddress.IPv4Network, ipaddress.IPv6Network], dst: Union[ipaddress.IPv4Network, ipaddress.IPv6Network], mtu: int, trigger: int) -> None:
+        self.src = src
+        self.dst = dst
+        self.mtu = mtu
+        self.trigger = trigger
+
+    def match(self, src: Union[ipaddress.IPv4Address, ipaddress.IPv6Address], dst: Union[ipaddress.IPv4Address, ipaddress.IPv6Address]) -> bool:
+        return src in self.src and dst in self.dst
+
+    def mtu_upperbound(self, src: Union[ipaddress.IPv4Address, ipaddress.IPv6Address], dst: Union[ipaddress.IPv4Address, ipaddress.IPv6Address], previous_mtu: Optional[int], previous_trigger: Optional[int]) -> Tuple[Optional[int], Optional[int]]:
+        if src in self.src and dst in self.dst:
+            return self.mtu if previous_mtu is None else min(self.mtu, previous_mtu), self.trigger if previous_trigger is None else min(self.mtu, previous_trigger)
+        return previous_mtu, previous_trigger
+
+
+def print_usage(program_name: str) -> None:
+    print('Usage: {} config_file'.format(program_name))
+    print()
+    print('Configuration file format:')
+    print('    iface  <INTERFACE NAME>')
+    print('    filter <LIBPCAP FILTER>')
+    print('    cache  <CACHE ITEMS> <CACHE SECONDS>')
+    print('    <SRC CIDR 1> <DST CIDR 1> <MTU 1> [ <TRIGGER LENGTH 1> ]')
+    print('    <SRC CIDR 2> <DST CIDR 2> <MTU 2> [ <TRIGGER LENGTH 2> ]')
+
+
+def callback(rules: List[Rule], pmtud_cache: MutableMapping[str, None], packet: scapy.packet.Packet) -> Optional[str]:
+    if not isinstance(packet, (scapy.layers.inet.IP, scapy.layers.inet6.IPv6)):
+        packet = packet.payload
+    mtu, trigger = None, None  # type: Tuple[Optional[int], Optional[int]]
+    if isinstance(packet, scapy.layers.inet.IP):
+        if packet.proto == 1 and packet.payload.type not in (0, 8):
+            return None
+        src = ipaddress.IPv4Address(packet.src)
+        dst = ipaddress.IPv4Address(packet.dst)
+        if packet.src in pmtud_cache:
+            return None
+        for rule in rules:
+            mtu, trigger = rule.mtu_upperbound(src, dst, mtu, trigger)
+        raw_packet = bytes(packet)
+        if mtu is not None and trigger is not None and trigger < min(len(raw_packet), packet.len):
+            logging.info('{} -\u2192 {} MTU {}'.format(packet.src, packet.dst, mtu))
+            reply = scapy.layers.inet.IP(id=packet.id, ttl=64, src=packet.dst, dst=packet.src) / scapy.layers.inet.ICMP(type=3, code=4, nexthopmtu=mtu) / raw_packet[:28]
+            pmtud_cache[packet.src] = None
+            try:
+                scapy.sendrecv.send(reply, verbose=False)
+            except Exception as e:
+                logging.error('Error sending packet: {}'.format(e))
+                return None
+    elif isinstance(packet, scapy.layers.inet6.IPv6):
+        if packet.nh == 58 and packet.payload.type < 128:
+            return None
+        src = ipaddress.IPv6Address(packet.src)
+        dst = ipaddress.IPv6Address(packet.dst)
+        if packet.src in pmtud_cache:
+            return None
+        for rule in rules:
+            mtu, trigger = rule.mtu_upperbound(src, dst, mtu, trigger)
+        raw_packet = bytes(packet)
+        if mtu is not None and trigger is not None and trigger < len(raw_packet):
+            logging.info('{} -\u2192 {} MTU {}'.format(packet.src, packet.dst, mtu))
+            reply = scapy.layers.inet6.IPv6(hlim=64, src=packet.dst, dst=packet.src) / scapy.layers.inet6.ICMPv6PacketTooBig(mtu=mtu) / raw_packet[:max(mtu - 48, 1232)]
+            pmtud_cache[packet.src] = None
+            try:
+                scapy.sendrecv.send(reply, verbose=False)
+            except Exception as e:
+                logging.error('Error sending packet: {}'.format(e))
+                return None
+    return None
+
+
+def main(argv: List[str]) -> int:
+    logging.basicConfig(format='[%(asctime)s] %(message)s', level=logging.INFO)
+    if len(argv) != 2:
+        print_usage(argv[0])
+        return 0
+    cache_items, cache_seconds = 1024, 10
+    iface = []  # type: List[str]
+    bpf = None  # type: Optional[str]
+    rules = []  #type: List[Rule]
+    with open(argv[1], 'r') as config_file:
+        for lineno, line in enumerate(config_file):
+            fields = line.split('#', 1)[0].strip().split()
+            if len(fields) == 0:
+                continue
+            if fields[0] == 'iface':
+                if len(fields) != 2:
+                    logging.error('Error in line {}: {}'.format(lineno + 1, line))
+                    return 1
+                iface.append(fields[1])
+            elif fields[0] == 'filter':
+                bpf = ' '.join(fields[1:])
+            elif fields[0] == 'cache':
+                if len(fields) != 3:
+                    logging.error('Error in line {}: {}'.format(lineno + 1, line))
+                    return 1
+                try:
+                    cache_items = int(fields[1])
+                    if cache_items <= 0:
+                        raise ValueError('Cache items should be greater than 0')
+                    cache_seconds = int(fields[2])
+                    if cache_seconds <= 0:
+                        raise ValueError('Cache seconds should be greater than 0')
+                except Exception as e:
+                    logging.error('Error in line {}: {}'.format(lineno + 1, e))
+                    return 1
+            else:
+                if len(fields) not in (3, 4):
+                    logging.error('Error in line {}: {}'.format(lineno + 1, line))
+                try:
+                    src = ipaddress.ip_network(fields[0])
+                    dst = ipaddress.ip_network(fields[1])
+                    mtu = int(fields[2])
+                    if mtu <= 0:
+                        raise ValueError('MTU should be greater than 0')
+                    trigger = mtu
+                    if len(fields) >= 4:
+                        trigger = int(fields[3])
+                        if trigger < 0:
+                            raise ValueError('Trigger length should be greater or equal than 0')
+                except Exception as e:
+                    logging.error('Error in line {}: {}'.format(lineno + 1, e))
+                    return 1
+                rules.append(Rule(src, dst, mtu, trigger))
+
+    pmtud_cache = expiringdict.ExpiringDict(max_len=cache_items, max_age_seconds=cache_seconds)
+
+    try:
+        scapy.sendrecv.sniff(prn=functools.partial(callback, rules, pmtud_cache), promisc=False, filter=bpf, iface=iface or None)
+    except Exception as e:
+        logging.error('Error sniffing packets: {}'.format(e))
+        return 1
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
